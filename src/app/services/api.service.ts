a55845a16fb3ea5e7ad05f0281c8fef8e3de6f9b
wala <wala@example.com>
2025-08-13 20:47:49 +0800

    asdf

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -43,11 +43,7 @@ interface SignConsentResponse {
   providedIn: 'root'
 })
 export class ApiService {
-<<<<<<< HEAD
-  private apiUrl = environment.apiUrl || 'http://172.20.10.3:8000/api';
-=======
-  private apiUrl = environment.apiUrl || 'http://192.168.1.12:8000/api';
->>>>>>> aac54962b1e75f70b57da41b153a55581ea18ac5
+  private apiUrl = environment.apiUrl || 'http://192.168.42.97:8000/api';
 
   // User management
   private currentUserSubject = new BehaviorSubject<User | null>(null);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  apiUrl: 'http://172.20.10.3:8000/api',
-=======
-  apiUrl: 'http://192.168.1.12:8000/api',
->>>>>>> aac54962b1e75f70b57da41b153a55581ea18ac5
+  apiUrl: 'http://192.168.42.97:8000/api',
 };
 
 /*
