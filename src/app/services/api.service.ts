9298a99d8801c70559d13e4c8e3394485e97dbf0
wala <wala@example.com>
2026-03-15 10:49:26 +0800

    trash

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -43,7 +43,7 @@ interface SignConsentResponse {
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = environment.apiUrl || 'http://192.168.1.14:8000/api';
+  private apiUrl = environment.apiUrl || 'http://10.141.1.19:8000/api';
 
   // User management
   private currentUserSubject = new BehaviorSubject<User | null>(null);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.14:8000/api', 
+  apiUrl: 'http://10.141.100.132:8000/api', 
 };
